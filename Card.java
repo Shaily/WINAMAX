cca443e5d01ccc6795cfdb695d4b17cb30842fa7
Shaily <shaily.mehta871@gmail.com>
2014-12-24 21:53:50 +0800

    Create Card.java

---
+++ Card.java
@@ -0,0 +1,19 @@
+
+public class Card {
+	
+		String value;
+
+		public String getValue() {
+			return value;
+		}
+
+		public Card(String value) {
+			this.value = value;
+		}
+
+		public void setValue(String value) {
+			this.value = value;
+		}
+	}
+
+
